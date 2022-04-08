4fad00294f527b2268c7951abeffa1a71b9fb505
Felipe Padilla <81942618+UNDERSTRAKER@users.noreply.github.com>
2022-04-08 09:09:43 -0500

    Add files via upload

---
+++ Boton.ts
@@ -0,0 +1,21 @@
+export default class Boton {
+    constructor(p){
+        this.p = p;
+        this.pos = 100;
+        this.tamano = 80;
+    }
+
+    mostrar(): void {
+        
+        for (let fila = 0; fila < 4; fila++) {
+            for (let col = 0; col < 5; col++) {
+                this.p.fill('orange');
+                this.p.rect(80*fila,80*col,this.tamano,this.tamano);
+            }
+        }
+        
+    }
+
+
+
+}
---
+++ index.ts
@@ -0,0 +1,16 @@
+
+let sketch = function (p:any) {
+    let boton = new Boton(p);
+    p.setup = function(){
+        p.createCanvas(500,450);
+        
+    };
+
+    p.draw = function(){
+        p.background(0);
+        boton.mostrar();
+
+    };  
+};
+
+let myp5 = new p5(sketch);
